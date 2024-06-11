5e59e0dc3eb98ea21cb32bfcc7ce48c9b5bc9443
Upal Bhattacharya <u.bhattacharya@uu.nl>
2024-06-11 16:05:27 +0200

    Update pyproject.toml. Update requirements.txt. Update sub-module names. Add response_formatting sub-package.

--- src/llm_ontology_awareness/prompt_generation/individual_to_class.py
+++ src/llm_ontology_awareness/prompt_generation/individual_to_class.py
@@ -2,11 +2,7 @@
 
 # Script for various forms of prompting Individual -> Concept/Class mapping
 
-import itertools
 import random
-
-import more_itertools
-import owlready2
 
 random.seed(42)
 
@@ -14,21 +10,9 @@ random.seed(42)
 class IndividualToClassPrompts:
     """Generate various individual to class mapping prompts"""
 
-    def __init__(self, ontologyPath: str):
-        onto = owlready2.get_ontology(ontologyPath).load(only_local=True)
-        # Get correct namespace to use for access
-        namespaces = list(onto._namespaces.keys())
-        for n in namespaces:
-            namespace = owlready2.get_namespace(n)
-            if namespace[more_itertools.first_true(onto.classes()).name] is not None:
-                break
-
-        self.namespace = namespace
-        self.namespace.ontology = onto.ontology
-        self.individuals = list(self.namespace.ontology.individuals())
-        self.classes = list(self.namespace.ontology.classes())
-        self.indClsPairs = list(itertools.product(self.individuals, self.classes))
-        random.shuffle(self.indClsPairs)
+    def __init__(self, targetFile: str):
+        with open(targetFile, "r") as f:
+            individual_classes = json.load(f)
 
     def noStructureZeroShot(self) -> str:
         """Create zero shot, no hierarchy information context prompts"""
