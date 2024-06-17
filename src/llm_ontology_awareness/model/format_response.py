1ddee523cfcb30463d9e1ebbd7d9f3a29a797a8a
Upal Bhattacharya <u.bhattacharya@uu.nl>
2024-06-17 15:26:06 +0200

    (WIP) Change `re.find()` to `re.search()`.

--- src/llm_ontology_awareness/model/format_response.py
+++ src/llm_ontology_awareness/model/format_response.py
@@ -9,7 +9,7 @@ def binary_classify(response) -> bool:
         "false": False,
     }
     pattern = re.compile(r"(true|false)", re.MULTILINE)
-    return value_map.get(re.find(pattern, response).group(0))
+    return value_map.get(re.search(pattern, response).group(0))
 
 
 format_types = {"binary_classify": binary_classify}
