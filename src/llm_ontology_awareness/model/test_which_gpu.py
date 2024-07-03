56e5413c0478ecf54deac8eb857ccf4c8510562d
Upal Bhattacharya <u.bhattacharya@uu.nl>
2024-07-03 16:52:10 +0200

    (WIP) Change `CUDA_VISIBLE_GPUS` to `CUDA_VISIBLE_DEVICES`

--- src/llm_ontology_awareness/model/test_which_gpu.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-import torch
-
-print(f"Is GPU available? {torch.cuda.is_available()}")
-if torch.cuda.is_available():
-    print(f"CUDA device ID: {torch.cuda.current_device()}")
