2f0f156dfccc3015fb0cf0112ce2527e9d2f5e2d
Upal Bhattacharya <u.bhattacharya@uu.nl>
2024-07-03 15:32:40 +0200

    Fix `test_which_gpu.py`

--- src/llm_ontology_awareness/model/test_which_gpu.py
+++ src/llm_ontology_awareness/model/test_which_gpu.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 
 import torch
-import logging
 
-logging.info(f"Is GPU available? {torch.cuda.is_available()}")
+print(f"Is GPU available? {torch.cuda.is_available()}")
 if torch.cuda.is_available():
-    logging.info(f"CUDA device ID: {torch.cuda.current_device()}")
+    print(f"CUDA device ID: {torch.cuda.current_device()}")
