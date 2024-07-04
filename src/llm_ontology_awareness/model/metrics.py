7be08d7774d750507113166a8565635487185ec4
Upal Bhattacharya <u.bhattacharya@uu.nl>
2024-07-04 11:16:25 +0200

    Add additional metrics

--- src/llm_ontology_awareness/model/metrics.py
+++ src/llm_ontology_awareness/model/metrics.py
@@ -2,7 +2,7 @@
 
 from typing import Union
 
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, average_precision_score, confusion_matrix
 from sklearn.metrics import precision_recall_fscore_support as prfs
 
 
@@ -11,10 +11,17 @@ def binary_classify(
 ) -> dict[float, float, float, Union[float, None]]:
     p, r, f, s = prfs(y_true, y_pred, average="binary")
     a = accuracy_score(y_true, y_pred)
+    ap = average_precision_score(y_true, y_pred)
+    tn, fp, fn, tp = confusion_matrix(y_true, y_pred).ravel()
+    spec = tn / (tn + fp)
+    fnr = fn / (fn + tp)
     return {
         "precision": p,
         "recall": r,
+        "specificity": spec,
+        "fnr": fnr,
         "f1": f,
+        "average_precision": ap,
         "accuracy": a,
         "support": s,
     }
