46aaad8374f760b225f2dda2cb06855b20bd5a93
upalbhattacharya <upal.bhattacharya@gmail.com>
2024-10-11 16:14:51 +0200

    shell.nix

--- shell.nix
+++ shell.nix
@@ -1,11 +1,25 @@
 let
-pkgs = import <nixpkgs> {};
+  # We pin to a specific nixpkgs commit for reproducibility.
+  # Last updated: 2024-04-29. Check for new commits at https://status.nixos.org.
+  pkgs = import <nixpkgs> { };
 in pkgs.mkShell {
   packages = [
-    (pkgs.python3.withPackages (python-pkgs: with python-pkgs; [
-      # select Python packages here
-      pandas
-      requests
-    ]))
+    (pkgs.python3.withPackages (python-pkgs:
+      with python-pkgs; [
+        # select Python packages here
+        ontospy
+        openai
+        polar
+        pydantic
+        python-dotenv
+        scikit-learn
+        torch
+        tqdm
+        transformers
+        black
+        isort
+        pyright
+        flake8
+      ]))
   ];
-}}
+}
