98f9b810bdd1ae05f2ddbf3746c44dc95360cce9
upalbhattacharya <upal.bhattacharya@gmail.com>
2024-10-11 16:27:05 +0200

    shell.nix

--- shell.nix
+++ shell.nix
@@ -2,6 +2,7 @@ let
   # We pin to a specific nixpkgs commit for reproducibility.
   # Last updated: 2024-04-29. Check for new commits at https://status.nixos.org.
   pkgs = import <nixpkgs> { };
+  python = pkgs.python310;
 in pkgs.mkShell {
   packages = [
     (pkgs.python3.withPackages (python-pkgs:
