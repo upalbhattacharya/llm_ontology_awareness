b784084a4b2a2adc5c42a34577898bb29b5c50ef
Upal Bhattacharya <u.bhattacharya@uu.nl>
2024-10-21 15:41:20 +0200

    shell.nix

--- shell.nix
+++ shell.nix
@@ -1,26 +1,88 @@
-let
-  pkgs = import <nixpkgs> { };
-  python = pkgs.python312;
-in
-pkgs.mkShell {
-  packages = [
-    (python.withPackages (
-      python-pkgs: with python-pkgs; [
-        openai
-        polars
-        pydantic
-        python-dotenv
-        scikit-learn
-        torch
-        tqdm
-        transformers
-        black
-        isort
-        flake8
-      ]
-    ))
-    pkgs.pyright
-    pkgs.nixd
-    pkgs.nixfmt-rfc-style
-  ];
+{
+  description = "Zero shot term typing using LLMs";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.systems.url = "github:nix-systems/default";
+  inputs.flake-utils = {
+    url = "github:numtide/flake-utils";
+    inputs.systems.follows = "systems";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        python = pkgs.python312;
+
+        # Build Packages 
+        buildPythonPackges = (
+          python.withPackages (
+            ps: with ps; [
+              setuptools
+            ]
+          )
+        );
+
+        # Python modules for actual package
+        modulePythonPackages = (
+          python.withPackages (
+            ps:
+            with ps;
+            [
+            ]
+          )
+        );
+
+        # Python development packages used for development
+        # LSP, formatting, etc.
+        devPythonPackages = (
+          python.withPackages (
+            ps: with ps; [
+              python-lsp-server
+              isort
+              black
+              flake8
+            ]
+          )
+        );
+
+
+        # Other development packages available in the nixpkgs
+        devPackages = (
+          with pkgs;
+          [
+            nixd
+            nixfmt-rfc-style
+          ]
+        );
+
+        # The main module
+        myapp = pkgs.python3Packages.buildPythonPackage rec {
+          # Change name here
+          pname = "llm_zero_shot_term_typing_impact";
+          pyproject = true;
+          version = "0.1.0";
+          src = ./.;
+          build-system = [
+            buildPythonPackages
+            modulePythonPackages
+            
+          ];
+        };
+
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.bashInteractive
+            devPackages
+            devPythonPackages
+            packagePythonPackages
+            myapp
+          ];
+        };
+      }
+    );
 }
