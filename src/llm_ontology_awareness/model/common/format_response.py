e9ae52387ccf38a3aa76fdead3680c61dc9f4bb4
Upal Bhattacharya <u.bhattacharya@uu.nl>
2024-10-30 20:43:05 +0100

    src/llm_ontology_awareness/model/common/format_response.py

--- src/llm_ontology_awareness/model/common/format_response.py
+++ src/llm_ontology_awareness/model/common/format_response.py
@@ -20,8 +20,9 @@ def binary_classify(response: str) -> bool:
 
 def ranked_retrieval(response: str) -> list:
     ranks = list(filter(None, response.split("\n")))
+    ranks = [re.sub(r"[[']]", "", item) for item in ranks]
     ranks = [item for item in ranks if re.match(r"^\d", item)]
 
-    pattern = re.compile(r"^\d+.*\s+(.*)", re.MULTILINE)
-    items = [re.search(pattern, r).group(1) for r in ranks]
+    pattern = re.compile(r"(^\d+)?.*\s+(.*)", re.MULTILINE)
+    items = [re.search(pattern, r).group(2) for r in ranks]
     return items
