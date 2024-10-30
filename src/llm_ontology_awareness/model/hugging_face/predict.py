5d5a2ebb3e1d93041bc87c85f60c12600cf0b3c8
Upal Bhattacharya <u.bhattacharya@uu.nl>
2024-10-30 17:11:45 +0100

    src/llm_ontology_awareness/model/hugging_face/predict.py

--- src/llm_ontology_awareness/model/hugging_face/predict.py
+++ src/llm_ontology_awareness/model/hugging_face/predict.py
@@ -21,7 +21,7 @@ def predict(model, tokenizer, test_data, run_args, **kwargs) -> pl.DataFrame:
             tokenized.input_ids, max_new_tokens=run_args.max_tokens
         ).cpu()
         response = tokenizer.batch_decode(response)[0]
-        responses.append((inst, cl, response.replace(prompt, "")))
+        responses.append((f"task-{i}", response.replace(prompt, "")))
 
         if kwargs.get("stop", None) is not None and i == kwargs["stop"]:
             break
@@ -35,9 +35,7 @@ def predict(model, tokenizer, test_data, run_args, **kwargs) -> pl.DataFrame:
         ],
     )
 
-    df = pl.DataFrame(
-        responses, schema=[("Individual", str), ("Class", str), ("Response", str)]
-    )
+    df = pl.DataFrame(responses, schema=[("Custom ID", str), ("Response", str)])
 
     df = df.with_columns(
         pl.col("Response")
