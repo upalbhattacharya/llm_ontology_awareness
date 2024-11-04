93aa0392af5071e6851d3b21958c6326285cb10a
Upal Bhattacharya <u.bhattacharya@uu.nl>
2024-11-04 16:36:56 +0100

    src/llm_ontology_awareness/model/open_ai/predict.py

--- src/llm_ontology_awareness/model/open_ai/predict.py
+++ src/llm_ontology_awareness/model/open_ai/predict.py
@@ -5,8 +5,6 @@ from llm_ontology_awareness.model.open_ai.datasets.term_typing import (
     TermTypingRankedRetrievalDataset,
 )
 from openai import OpenAI
-
-
 from tqdm import tqdm
 
 load_dotenv()
@@ -15,9 +13,6 @@ load_dotenv()
 def predict(dataset, run_args):
 
     client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
-
-    for inst, message, label in 
-    pass
 
 
 if __name__ == "__main__":
