c219ef304ddf29bd64d2315caca48c87c6e07a7d
Upal Bhattacharya <u.bhattacharya@uu.nl>
2024-11-04 16:29:48 +0100

    src/llm_ontology_awareness/model/open_ai/predict.py

---
+++ src/llm_ontology_awareness/model/open_ai/predict.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from dotenv import load_dotenv
+from llm_ontology_awareness.model.open_ai.datasets.term_typing import (
+    TermTypingRankedRetrievalDataset,
+)
+from openai import OpenAI
+
+
+def predict(**kwargs):
+    pass
+
+
+if __name__ == "__main__":
+    import argparse
