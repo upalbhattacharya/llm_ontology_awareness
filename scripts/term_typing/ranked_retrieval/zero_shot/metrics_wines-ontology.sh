ff82fa810072b22e05c4562c380ba398e5868802
Upal Bhattacharya <u.bhattacharya@uu.nl>
2024-11-05 14:34:17 +0100

    scripts/term_typing/ranked_retrieval/zero_shot/metrics_wines-ontology.sh

---
+++ scripts/term_typing/ranked_retrieval/zero_shot/metrics_wines-ontology.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env sh
+
+DEPTH=4
+Y_TRUE="~/Data/ontologies/astronomy-ontology/data/term_typing/ranked_retrieval/2024-11-04/term_typing_ranked_retrieval_dataset.json"
+RESULTS_DIR="~/Results/llm_ontology_awareness/term_typing_ranked_retrieval/zero_shot/wines-ontology"
+
+# Check for environment
+echo $(which python)
+
+# GPT-4o
+
+python src/llm_ontology_awareness/metrics/results/term_typing.py \
+       -yt $Y_TRUE \
+       -yp $RESULTS_DIR/gpt-4o/34be5cbb-9f49-4e85-af5d-9c2444fb66e2/runs/run_1/responses.json \
+       -n ranked_retrieval \
+       -k k=$DEPTH
+
+python src/llm_ontology_awareness/metrics/results/term_typing.py \
+       -yt $Y_TRUE \
+       -yp $RESULTS_DIR/gpt-4o/520aa919-1326-4f1d-bb46-bde3ef86a74d/runs/run_1/responses.json \
+       -n ranked_retrieval \
+       -k k=$DEPTH
+
+python src/llm_ontology_awareness/metrics/results/term_typing.py \
+       -yt $Y_TRUE \
+       -yp $RESULTS_DIR/gpt-4o/5b1111cc-902b-465b-9e4f-257774be59b0/runs/run_1/responses.json \
+       -n ranked_retrieval \
+       -k k=$DEPTH
+
+python src/llm_ontology_awareness/metrics/results/term_typing.py \
+       -yt $Y_TRUE \
+       -yp $RESULTS_DIR/gpt-4o/5d599257-8bf3-4470-96ff-4f344bcc6947/runs/run_1/responses.json \
+       -n ranked_retrieval \
+       -k k=$DEPTH
+
+# o1-preview
+
+
+
+# Llama3-7B
+
