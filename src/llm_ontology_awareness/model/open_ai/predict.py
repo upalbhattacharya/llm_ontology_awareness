5258f4aead1a441c70428f0d73c86cae2790ecdf
Upal Bhattacharya <u.bhattacharya@uu.nl>
2024-11-05 11:46:00 +0100

    src/llm_ontology_awareness/model/open_ai/predict.py

--- src/llm_ontology_awareness/model/open_ai/predict.py
+++ src/llm_ontology_awareness/model/open_ai/predict.py
@@ -50,6 +50,7 @@ def predict(test_data, run_args, **kwargs) -> Union[Dict, pl.DataFrame]:
         responses.append(completion)
 
         if kwargs.get("stop", -1) != -1 & kwargs["stop"] == i:
+            print(i)
             break
 
     label_mapping_df = pl.DataFrame(
