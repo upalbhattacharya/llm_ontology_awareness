4180d7ca43c39f68f1fd3223d70256a5735abcb1
Upal Bhattacharya <u.bhattacharya@uu.nl>
2024-11-05 11:50:48 +0100

    src/llm_ontology_awareness/model/open_ai/predict.py

--- src/llm_ontology_awareness/model/open_ai/predict.py
+++ src/llm_ontology_awareness/model/open_ai/predict.py
@@ -49,8 +49,7 @@ def predict(test_data, run_args, **kwargs) -> Union[Dict, pl.DataFrame]:
         completion["custom_id"] = f"task-{i}"
         print(completion["choices"][0].message.content)
         responses.append(completion)
-
-        if kwargs.get("stop", -1) != -1 && kwargs["stop"] == i:
+        if (kwargs.get("stop", -1) != -1) & (kwargs["stop"] == i):
             print(i)
             break
 
