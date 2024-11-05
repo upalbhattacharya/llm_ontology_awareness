1cec87d76b4c4982ad4d8d2ca32a28cae9bddef5
Upal Bhattacharya <u.bhattacharya@uu.nl>
2024-11-05 10:40:49 +0100

    src/llm_ontology_awareness/model/open_ai/predict.py

--- src/llm_ontology_awareness/model/open_ai/predict.py
+++ src/llm_ontology_awareness/model/open_ai/predict.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 """Prediction script for non-Batch API models"""
 
+import jsonlines
 from dotenv import load_dotenv
 from llm_ontology_awareness.model.open_ai.datasets.term_typing import (
     TermTypingRankedRetrievalDataset,
@@ -16,6 +17,7 @@ def predict(test_data, run_args, **kwargs) -> None:
     client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
     num_samples = len(test_data)
     test_data = iter(test_data)
+    responses = []
     for i in tqdm(range(num_samples)):
         inst, prompt, label = next(test_data)
         completion = client.chat.completions.create(
@@ -23,7 +25,6 @@ def predict(test_data, run_args, **kwargs) -> None:
             max_completion_tokens=run_args.max_tokens,
             messages=prompt,
         )
-        pass
 
 
 if __name__ == "__main__":
