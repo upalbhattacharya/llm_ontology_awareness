8f3170ad83f819894a97b479b4034f260ceb2782
Upal Bhattacharya <u.bhattacharya@uu.nl>
2024-11-05 11:48:16 +0100

    src/llm_ontology_awareness/model/open_ai/predict.py

--- src/llm_ontology_awareness/model/open_ai/predict.py
+++ src/llm_ontology_awareness/model/open_ai/predict.py
@@ -16,7 +16,7 @@ load_dotenv()
 
 
 def predict(test_data, run_args, **kwargs) -> Union[Dict, pl.DataFrame]:
-    print(kwargs)
+    print(kwargs["stop"])
 
     client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
     responses = []
