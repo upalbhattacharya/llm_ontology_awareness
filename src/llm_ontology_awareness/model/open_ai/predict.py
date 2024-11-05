81e794334cc25843b2e1719773ec9b90ff16cbc3
Upal Bhattacharya <u.bhattacharya@uu.nl>
2024-11-05 13:08:04 +0100

    src/llm_ontology_awareness/model/open_ai/predict.py

--- src/llm_ontology_awareness/model/open_ai/predict.py
+++ src/llm_ontology_awareness/model/open_ai/predict.py
@@ -44,6 +44,7 @@ def predict(test_data, run_args, **kwargs) -> Union[Dict, pl.DataFrame]:
                 messages=prompt,
             )
         completion = dict(completion)
+        
         completion["custom_id"] = f"task-{i}"
         responses.append(completion)
         if (kwargs.get("stop", -1) != -1) & (kwargs["stop"] == i):
@@ -110,7 +111,8 @@ if __name__ == "__main__":
 
     label_mapping_df, responses = predict(test_data, run_args, stop=0)
     label_mapping_df.write_ndjson(os.path.join(output_dir, "label_mapping.json"))
-    with jsonlines.open(
-        os.path.join(output_dir, "responses.jsonl"), mode="w"
-    ) as writer:
-        writer.write_all(responses)
+    with open(
+        os.path.join(output_dir, "responses.jsonl"), "a"
+    ) as f:
+        for obj in responses:
+            json.dumps(obj, indent=4, f)
