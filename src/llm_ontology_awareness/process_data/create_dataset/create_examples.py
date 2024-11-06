924b4faeb444961308f8c548a878ff5007bafa00
Upal Bhattacharya <u.bhattacharya@uu.nl>
2024-11-06 16:01:04 +0100

    src/llm_ontology_awareness/process_data/create_dataset/create_examples.py

--- src/llm_ontology_awareness/process_data/create_dataset/create_examples.py
+++ src/llm_ontology_awareness/process_data/create_dataset/create_examples.py
@@ -2,6 +2,7 @@
 
 import polars as pl
 import argparse
+import json
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", "-file", type=str,
@@ -10,26 +11,31 @@ parser.add_argument("-o", "--output_dir", type=str,
                     help="Path to store generated output")
 parser.add_argument("-m", "--metrics", type=str,
                     help="Path to metrics dictionary")
+parser.add_argument("-k", "--count", type=int, default=2
+                    help="Number of examples to select")
 
 args = parser.parse_args()
-
 key = "class_counts"
 
+with open(args.metrics, "r") as f:
+    metrics = json.load(f)
 
-        date_dir = datetime.now().strftime("%Y-%m-%d")
-        final_dir = out_dir
-        count = sum([x.startswith(date_dir) for x in os.listdir(out_dir)])
-        final_dir = (
-            Path(final_dir) / f"{date_dir}.{count}"
-            if count != 0
-            else Path(final_dir) / date_dir
-        )
-        if not os.path.exists(final_dir):
-            os.makedirs(final_dir)
+    
 
-        df = pl.DataFrame(
-            ranked_entries,
-            schema=[("Individual", str), ("Ranked List", list[str])],
-        )
+date_dir = datetime.now().strftime("%Y-%m-%d")
+final_dir = out_dir
+count = sum([x.startswith(date_dir) for x in os.listdir(out_dir)])
+final_dir = (
+    Path(final_dir) / f"{date_dir}.{count}"
+    if count != 0
+    else Path(final_dir) / date_dir
+)
+if not os.path.exists(final_dir):
+    os.makedirs(final_dir)
 
-        df.write_ndjson(Path(final_dir) / "term_typing_ranked_retrieval_dataset.json")
+df = pl.DataFrame(
+    ranked_entries,
+    schema=[("Individual", str), ("Ranked List", list[str])],
+)
+
+df.write_ndjson(Path(final_dir) / "term_typing_ranked_retrieval_dataset.json")
