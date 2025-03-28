7c2d213c2b357fa40351ebcd6ac738eac9c4b97a
upalbhattacharya <upal.bhattacharya@gmail.com>
2025-03-28 10:49:11 +0100

    src/llm_ontology_awareness/model/hugging_face/datasets/term_typing.py

--- src/llm_ontology_awareness/model/hugging_face/datasets/term_typing.py
+++ src/llm_ontology_awareness/model/hugging_face/datasets/term_typing.py
@@ -46,6 +46,7 @@ class TermTypingRankedRetrievalDataset(Dataset):
         system_message: str,
         user_prompt_template: str,
         task_type: str,
+        examples_file: Optional[str] = None,
         **kwargs,
     ):
         self.task_type = task_type
@@ -54,6 +55,9 @@ class TermTypingRankedRetrievalDataset(Dataset):
                 f"`task_type` must be one of: {list(task_types.keys())}. Got {self.task_type}"
             )
         self.df = pl.read_ndjson(in_file)
+        self.examples = (
+            pl.read_ndjson(examples_file) if examples_file is not None else None
+        )
         self.system_message: str = system_message
         self.user_prompt_template: str = user_prompt_template
         self.extra_args = kwargs
@@ -66,9 +70,19 @@ class TermTypingRankedRetrievalDataset(Dataset):
     def __len__(self):
         return self.df.select(pl.len()).item()
 
+    def generate_examples(self):
+        example_print = ["---"]
+        for row in self.examples.rows():
+            example_print.append(row[0])
+            example_print.extend([f"{i+1}. {val}" for i, val in enumerate(row[1])])
+            example_print.append("\n")
+        return "\n".join(example_print)
+
     def __getitem__(self, idx):
         *ents, label = self.df.row(idx)
-        messages = f"""<|begin_of_text|><|start_header_id|>system<|end_header_id|>
+
+        if self.examples is not None:
+            messages = f"""<|begin_of_text|><|start_header_id|>system<|end_header_id|>
 
 {self.system_message.format(**self.extra_args, classes=self.classes)}<|eot_id|><|start_header_id|>user<|end_header_id|>
 
