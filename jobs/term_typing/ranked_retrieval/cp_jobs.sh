5f73d34bd4328d002f4eb1af1738c9bd2a8c9335
upalbhattacharya <upal.bhattacharya@gmail.com>
2025-03-29 21:12:33 +0100

    jobs/term_typing/ranked_retrieval/cp_jobs.sh

---
+++ jobs/term_typing/ranked_retrieval/cp_jobs.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+REF_JOB=~/PhD/Experiments/llm_ontology_awareness/jobs/term_typing/ranked_retrieval/0_shot/astronomy-ontology/llama3-7B/job_4c21158e-b2d3-4d74-b561-85befef94eab
+BASE_RUN_ARGS_PATH=~/PhD/Experiments/llm_ontology_awareness/run_args/term_typing/ranked_retrieval
+for i in {1..10}; do
+    for j in {astronomy-ontology,wines-ontology,case-uco-owl-trafficking}; do
+        for run_args in ${BASE_RUN_ARGS_PATH}/${i}_shot/${j}/llama3-7B/; do
+            echo ${run_args}
+        done
+    done
+done
+
+
