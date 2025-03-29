b5bd5bbce7e0b6fa95aa100ae79cd1e1adb698ea
upalbhattacharya <upal.bhattacharya@gmail.com>
2025-03-29 16:55:42 +0100

    run_args/term_typing/ranked_retrieval/cp_run_args.sh

--- run_args/term_typing/ranked_retrieval/cp_run_args.sh
+++ run_args/term_typing/ranked_retrieval/cp_run_args.sh
@@ -4,7 +4,7 @@ SRC_DIR="~/PhD/Experiments/llm_ontology_awareness/run_args/term_typing/ranked_re
 for i in {2..10}; do
     for j in {astronomy-ontology,case-uco-owl-trafficking,wines-ontology}; do
         cd ${i}_shot/most_common/${j}/llama3-7B
-        for x in $(${SRC_DIR}/${j}/llama3-7B/*.json); do
+        for x in $(${SRC_DIR}/${j}/llama3-7B/).*.json; do
             # cp ${x} $(uuidgen).json
             echo ${x}
         done
