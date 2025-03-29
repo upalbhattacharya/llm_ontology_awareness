8ed3e6c98d9e64b654c9bfc1454db13b5d3b55f0
upalbhattacharya <upal.bhattacharya@gmail.com>
2025-03-29 17:03:11 +0100

    run_args/term_typing/ranked_retrieval/cp_run_args.sh

--- run_args/term_typing/ranked_retrieval/cp_run_args.sh
+++ run_args/term_typing/ranked_retrieval/cp_run_args.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env sh
 
 SRC_DIR=~/PhD/Experiments/llm_ontology_awareness/run_args/term_typing/ranked_retrieval/1_shot/most_common
-PWD="$(pwd)"
 for i in {2..10}; do
     for j in {astronomy-ontology,case-uco-owl-trafficking,wines-ontology}; do
-        cd "$PWD/$i_shot/most_common/$j/llama3-7B"
+        cd "${i}_shot/most_common/${j}/llama3-7B"
         echo $(pwd)
         echo "---"
         for x in "$SRC_DIR/$j/llama3-7B"/*; do
