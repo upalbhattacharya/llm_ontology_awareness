9c4b53dd61bbf74e28d3fb10ae16b465c11567f5
upalbhattacharya <upal.bhattacharya@gmail.com>
2025-03-29 21:40:52 +0100

    jobs/term_typing/ranked_retrieval/cp_jobs.sh

--- jobs/term_typing/ranked_retrieval/cp_jobs.sh
+++ jobs/term_typing/ranked_retrieval/cp_jobs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-REF_JOB=~/PhD/Experiments/llm_ontology_awareness/jobs/term_typing/ranked_retrieval/0_shot/astronomy-ontology/llama3-7B/job_4c21158e-b2d3-4d74-b561-85befef94eab
+REF_JOB=~/PhD/Experiments/llm_ontology_awareness/jobs/term_typing/ranked_retrieval/sample_job
 BASE_RUN_ARGS_PATH=~/PhD/Experiments/llm_ontology_awareness/run_args/term_typing/ranked_retrieval
 BASE_JOBS_PATH=~/PhD/Experiments/llm_ontology_awareness/jobs/term_typing/ranked_retrieval
 for i in {1..10}; do
