4209e7c6d041de6f1daae9f2e611ad3b7ca44e15
upalbhattacharya <upal.bhattacharya@gmail.com>
2025-03-29 17:39:12 +0100

    Update incorrect data paths

--- run_args/term_typing/ranked_retrieval/cp_run_args.sh
+++ run_args/term_typing/ranked_retrieval/cp_run_args.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-SRC_DIR=~/PhD/Experiments/llm_ontology_awareness/run_args/term_typing/ranked_retrieval/1_shot/most_common
+SRC_DIR=~/PhD/Experiments/llm_ontology_awareness/run_args/term_typing/ranked_retrieval/1_shot/most_common/most_common
 for i in {2..10}; do
     for j in {astronomy-ontology,case-uco-owl-trafficking,wines-ontology}; do
         cd "${i}_shot/most_common/${j}/llama3-7B"
@@ -8,7 +8,7 @@ for i in {2..10}; do
         for x in "${SRC_DIR}/${j}/llama3-7B"/*; do
             cp ${x} $(uuidgen).json
         done
-        find . \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s/1_shot/${i}_shot/g"
+        find . \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s/1_shot/most_common/${i}_shot/g"
         find . \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s/1-Shot/${i}-Shot/g"
         cd ../../../..
         echo "---"
