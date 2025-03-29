a6cab4073833c262f21a6d6cb58a4d5845a12298
upalbhattacharya <upal.bhattacharya@gmail.com>
2025-03-29 16:59:36 +0100

    run_args/term_typing/ranked_retrieval/cp_run_args.sh

--- run_args/term_typing/ranked_retrieval/cp_run_args.sh
+++ run_args/term_typing/ranked_retrieval/cp_run_args.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-SRC_DIR="~/PhD/Experiments/llm_ontology_awareness/run_args/term_typing/ranked_retrieval/1_shot/most_common"
+SRC_DIR=~/PhD/Experiments/llm_ontology_awareness/run_args/term_typing/ranked_retrieval/1_shot/most_common
 for i in {2..10}; do
     for j in {astronomy-ontology,case-uco-owl-trafficking,wines-ontology}; do
         cd ${i}_shot/most_common/${j}/llama3-7B
