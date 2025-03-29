520f085834ebf669d6ba13987c27edb63b10fc73
upalbhattacharya <upal.bhattacharya@gmail.com>
2025-03-29 17:00:05 +0100

    run_args/term_typing/ranked_retrieval/cp_run_args.sh

--- run_args/term_typing/ranked_retrieval/cp_run_args.sh
+++ run_args/term_typing/ranked_retrieval/cp_run_args.sh
@@ -5,6 +5,7 @@ for i in {2..10}; do
     for j in {astronomy-ontology,case-uco-owl-trafficking,wines-ontology}; do
         cd "$i_shot/most_common/$j/llama3-7B"
         echo $(pwd)
+        echo "---"
         for x in "$SRC_DIR/$j/llama3-7B"/*; do
             echo ${x}
 
