3324b76fc6973bcbfdf882a1ffe32721c1577625
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-04-01 15:07:31 +0200

    src/llm_ontology_awareness/model/common/format_response.py

--- src/llm_ontology_awareness/model/common/format_response.py
+++ src/llm_ontology_awareness/model/common/format_response.py
@@ -26,7 +26,7 @@ def binary_classify(response: str) -> bool:
 
 def ranked_retrieval(response: str, llm_name: str) -> list:
     # assistant_response = response.split(llm_split_string[llm_name])[-1]
-    assistant_response = llm_split_string[llm_name].match(response).group(1)
+    assistant_response = llm_split_string[llm_name].search(response).group(1)
     print(assistant_response)
     print("=" * 40)
     ranks = list(filter(None, assistant_response.split("\n")))
