c0d806894043b9e66fc78cf27bfac502e72aa614
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-04-01 15:11:27 +0200

    src/llm_ontology_awareness/model/common/format_response.py

--- src/llm_ontology_awareness/model/common/format_response.py
+++ src/llm_ontology_awareness/model/common/format_response.py
@@ -26,7 +26,7 @@ def binary_classify(response: str) -> bool:
 
 def ranked_retrieval(response: str, llm_name: str) -> list:
     # assistant_response = response.split(llm_split_string[llm_name])[-1]
-    assistant_response = llm_split_string[llm_name].search(response).group(1)
+    # assistant_response = llm_split_string[llm_name].search(response).group(1)
     assistant_response = re.sub(
         r"<|start_header_id|>assistant<|end_header_id|>(.*)<|eot_id|>", r"\1", re.DOTALL
     )
