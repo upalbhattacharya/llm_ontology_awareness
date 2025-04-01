4d22498d4a0e8a4ba90e4ef1fa29c4a2203274c2
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-04-01 15:14:09 +0200

    src/llm_ontology_awareness/model/common/format_response.py

--- src/llm_ontology_awareness/model/common/format_response.py
+++ src/llm_ontology_awareness/model/common/format_response.py
@@ -29,7 +29,7 @@ def ranked_retrieval(response: str, llm_name: str) -> list:
     # assistant_response = llm_split_string[llm_name].search(response).group(1)
     assistant_response = re.sub(
         r"<\|start_header_id\|>assistant<\|end_header_id\|>(.*)<\|eot_id\|>",
-        r"Blah\1Blah",
+        r"Blah\1 Blah",
         response,
         flags=re.DOTALL,
     )
