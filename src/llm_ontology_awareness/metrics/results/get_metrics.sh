0230aa9ba0c66e5b54275ff8142409b3aa05bab0
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-04-03 12:24:38 +0200

    src/llm_ontology_awareness/metrics/results/get_metrics.sh

--- src/llm_ontology_awareness/metrics/results/get_metrics.sh
+++ src/llm_ontology_awareness/metrics/results/get_metrics.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ONT="astronomy-ontology"
-DEPTH=4
+DEPTH=10
 for i in $(seq 1 10); do
 	for j in "${HOME}/Results/llm_ontology_awareness/term_typing/ranked_retrieval/${i}_shot/most_common/${ONT}/llama3-7B"/*; do
 		./term_typing.py -yt ${HOME}/Data/ontologies/${ONT}/data/term_typing/ranked_retrieval/${i}_shot/most_common/2025-03-30/term_typing_ranked_retrieval_dataset.json -yp ${j}/run_2/predictions.json -n ranked_retrieval --kwargs k=${DEPTH}
