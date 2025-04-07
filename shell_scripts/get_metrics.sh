8dbdb72c299b975703a75017b21cdcd10224be06
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-04-07 16:23:03 +0200

    Generalize shell scripts. Update max_tokens for deepseek

--- shell_scripts/get_metrics.sh
+++ shell_scripts/get_metrics.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 
-ONT="case-uco-owl-trafficking"
-DEPTH=8
-for i in $(seq 1 10); do
-	for j in "${HOME}/Results/llm_ontology_awareness/term_typing/ranked_retrieval/${i}_shot/most_common/${ONT}/llama3-7B"/*; do
-		./term_typing.py -yt ${HOME}/Data/ontologies/${ONT}/data/term_typing/ranked_retrieval/${i}_shot/most_common/2025-03-30/term_typing_ranked_retrieval_dataset.json -yp ${j}/run_2/predictions.json -n ranked_retrieval --kwargs k=${DEPTH}
+ONT="wines-ontology"
+DEPTH=4
+DATE="2024-09-05"
+
+LLM="llama3-7B"
+SHOTS=(0)
+STRAT_SUFFIX=""
+# STRAT_SUFFIX="/most_common"
+# SHOTS=$(seq 1 10)
+
+RUN="run_1"
+
+for i in "${SHOTS[@]}"; do
+	for j in "${HOME}/Results/llm_ontology_awareness/term_typing/ranked_retrieval/${i}_shot${STRAT_SUFFIX}/${LLM}/${ONT}"/*; do
+		python3 ${HOME}/PhD/Experiments/llm_ontology_awareness/src/llm_ontology_awareness/metrics/results/term_typing.py -yt ${HOME}/Data/ontologies/${ONT}/data/term_typing/ranked_retrieval/${i}_shot${STRAT_SUFFIX}/${DATE}/term_typing_ranked_retrieval_dataset.json -yp ${j}/${RUN}/predictions.json -n ranked_retrieval --kwargs k=${DEPTH}
 	done
 done
 
