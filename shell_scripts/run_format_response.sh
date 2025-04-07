51319739a15b8c313fa5212f0e9b5d0ad5b5a1e5
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-04-07 14:39:47 +0200

    shell_scripts/run_format_response.sh

--- shell_scripts/run_format_response.sh
+++ shell_scripts/run_format_response.sh
@@ -2,14 +2,15 @@
 ARR=("astronomy-ontology" "case-uco-owl-trafficking" "wines-ontology")
 LLM="deepseekr1-distill-llama3-8B"
 shots=(0)
+RUN="run_1"
 for ont in "${ARR[@]}"; do
 	echo $ont
 	for i in "${shots[@]}"; do
 		for j in "${HOME}/Results/llm_ontology_awareness/term_typing/ranked_retrieval/${i}_shot/most_common/${ont}/${LLM}"/*; do
 			uid=$(basename "${j}")
 			echo ${uid}
-			./term_typing.py -f ${j}/run_2/responses.json \
-				-l ${j}/run_2/label_mapping.json \
+			./term_typing.py -f ${j}/${RUN}/responses.json \
+				-l ${j}/${RUN}/label_mapping.json \
 				-r "${HOME}/PhD/Experiments/llm_ontology_awareness/run_args/term_typing/ranked_retrieval/${i}_shot/most_common/${ont}/llama3-7B/${uid}.json"
 		done
 	done
