4028466fcb3625700f45b7562797b8f10d1b625b
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-04-08 13:35:09 +0200

    shell_scripts/cp_run_args.sh

--- shell_scripts/cp_run_args.sh
+++ shell_scripts/cp_run_args.sh
@@ -18,7 +18,6 @@ for i in "${SHOTS[@]}"; do
         done
         # find . \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s/1_shot/most_common/${i}_shot/g"
         # find . \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s/1-Shot/${i}-Shot/g"
-        cd ../../../..
         echo "---"
     done
 done
