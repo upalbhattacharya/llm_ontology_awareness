0e50958283268a628802d52164946f50449aa0a6
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-04-08 13:48:19 +0200

    shell_scripts/cp_run_args.sh

--- shell_scripts/cp_run_args.sh
+++ shell_scripts/cp_run_args.sh
@@ -12,16 +12,17 @@ STRAT_SUFFIX="/most_common"
 for i in "${SHOTS[@]}"; do
     for j in "${ONTOLOGIES[@]}"; do
         DEST_DIR="${RUN_ARGS_DIR}/${i}_shot${STRAT_SUFFIX}/${LLM}/${j}"
+        if [ "${DEST_DIR}" == "${SRC_RUN_ARGS_DIR}" ]; then
+            echo "Equal. Moving On"
+            continue
+        fi
         for x in "${SRC_RUN_ARGS_DIR}"/*; do
-            if [ "${DEST_DIR}" == "${SRC_RUN_ARGS_DIR}" ]; then
-                echo "Equal. Moving On"
-                continue
-            fi
             N_UUID=$(uuidgen)
             # echo "${DEST_DIR}/${N_UUID}.json"
             cp ${x} "${DEST_DIR}/${N_UUID}.json"
         done
         # find . \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s/1_shot/most_common/${i}_shot/g"
         # find . \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s/1-Shot/${i}-Shot/g"
+        sed -i '
     done
 done
