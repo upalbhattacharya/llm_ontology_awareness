dd16b86e5354d3f65655e3c9900633cf2e4c6d64
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-04-08 13:36:40 +0200

    shell_scripts/cp_run_args.sh

--- shell_scripts/cp_run_args.sh
+++ shell_scripts/cp_run_args.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-SRC_RUN_ARGS_DIR=$HOME/PhD/Experiments/llm_ontology_awareness/run_args/term_typing/ranked_retrieval/most_common/2_shot/gpt-4o/wines-ontology
+SRC_RUN_ARGS_DIR=$HOME/PhD/Experiments/llm_ontology_awareness/run_args/term_typing/ranked_retrieval/2_shot/most_common/gpt-4o/wines-ontology
 RUN_ARGS_DIR=$HOME/PhD/Experiments/llm_ontology_awareness/run_args/term_typing/ranked_retrieval
 
 LLM="gpt-4o"
