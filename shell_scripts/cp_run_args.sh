74b0c7c7a7379489722d5591f0af2513487029e9
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-04-08 13:25:03 +0200

    shell_scripts/cp_run_args.sh

--- shell_scripts/cp_run_args.sh
+++ shell_scripts/cp_run_args.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env sh
 
-SRC_DIR=~/PhD/Experiments/llm_ontology_awareness/run_args/term_typing/ranked_retrieval/1_shot/most_common/
+SRC_DIR=~/PhD/Experiments/llm_ontology_awareness/run_args/term_typing/ranked_retrieval/2_shot/most_common/
+LLM="gpt-4o"
 for i in {2..10}; do
     for j in {astronomy-ontology,case-uco-owl-trafficking,wines-ontology}; do
-        cd "${i}_shot/most_common/${j}/llama3-7B"
+        cd "${i}_shot/most_common/${LLM}/${j}"
         echo $(pwd)
-        for x in "${SRC_DIR}/${j}/llama3-7B"/*; do
-            cp ${x} $(uuidgen).json
+        for x in "${SRC_DIR}/${LLM}/${j}"/*; do
+            # cp ${x} $(uuidgen).json
         done
-        find . \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s/1_shot/most_common/${i}_shot/g"
-        find . \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s/1-Shot/${i}-Shot/g"
+        # find . \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s/1_shot/most_common/${i}_shot/g"
+        # find . \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s/1-Shot/${i}-Shot/g"
         cd ../../../..
         echo "---"
     done
