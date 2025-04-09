103c9801a9e4140edce529d1d1bb46d2a6d306c1
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-04-09 10:17:23 +0200

    shell_scripts/get_openai_batches.sh

--- shell_scripts/get_openai_batches.sh
+++ shell_scripts/get_openai_batches.sh
@@ -10,7 +10,7 @@ for shot in "${SHOTS[@]}"; do
     for ont in "${ONTOLOGIES[@]}"; do
         for r_args in "${BASE_PATH}/${shot}_shot${STRAT_SUFFIX}/${MODEL}/${ont}"/*; do
             # echo ${r_args}
-            python3 "${HOME}/PhD/Experiments/llm_ontology_awareness/src/llm_ontology_awareness/model/open_ai/batch/retrieve_response.py" "${r_args}/runs/run_1/ids.json"
+            python3 "${HOME}/PhD/Experiments/llm_ontology_awareness/src/llm_ontology_awareness/model/open_ai/batch/retrieve_response.py" -j "${r_args}/runs/run_1/ids.json"
         done 
     done
 done
