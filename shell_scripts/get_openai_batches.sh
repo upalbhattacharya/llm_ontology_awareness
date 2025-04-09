fb48659ad1e20cb171854ce5aa53903b6f73866f
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-04-09 10:55:40 +0200

    shell_scripts/get_openai_batches.sh

--- shell_scripts/get_openai_batches.sh
+++ shell_scripts/get_openai_batches.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-SHOTS=(1 3 5 10)
+SHOTS=(2 4 6 7 8 9)
 ONTOLOGIES=("astronomy-ontology" "case-uco-owl-trafficking" "wines-ontology")
 BASE_PATH="$HOME/Results/llm_ontology_awareness/term_typing/ranked_retrieval"
 MODEL="gpt-4o"
@@ -8,6 +8,10 @@ STRAT_SUFFIX="/most_common"
 
 for shot in "${SHOTS[@]}"; do
     for ont in "${ONTOLOGIES[@]}"; do
+        if [ ${shot} -eq 2 ] && [ ${ont} == "wines-ontology" ]; then
+            echo "Skipping"
+            continue
+        fi
         for r_args in "${BASE_PATH}/${shot}_shot${STRAT_SUFFIX}/${MODEL}/${ont}"/*; do
             # echo ${r_args}
             python3 "${HOME}/PhD/Experiments/llm_ontology_awareness/src/llm_ontology_awareness/model/open_ai/batch/retrieve_response.py" -j "${r_args}/runs/run_1/ids.json"
