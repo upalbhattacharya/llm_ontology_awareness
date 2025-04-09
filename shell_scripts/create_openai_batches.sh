f1c0c82cd38b2fa10149aa4ecf30d389e9f55799
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-04-09 10:35:56 +0200

    shell_scripts/create_openai_batches.sh

---
+++ shell_scripts/create_openai_batches.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+SHOTS=(2 4 6 7 8 9)
+ONTOLOGIES=("astronomy-ontology" "case-uco-owl-trafficking" "wines-ontology")
+BASE_PATH="$HOME/Results/llm_ontology_awareness/term_typing/ranked_retrieval"
+RUN_ARGS_PATH="$HOME/PhD/Experiments/llm_ontology_awareness/run_args/term_typing/ranked_retrieval"
+MODEL="gpt-4o"
+STRAT_SUFFIX="/most_common"
+
+for shot in "${SHOTS[@]}"; do
+    for ont in "${ONTOLOGIES[@]}"; do
+        for r_args in "${RUN_ARGS_PATH}/${shot}_shot${STRAT_SUFFIX}/${MODEL}/${ont}"/*; do
+            echo ${r_args}
+            # python3 "${HOME}/PhD/Experiments/llm_ontology_awareness/src/llm_ontology_awareness/model/open_ai/batch/submit_batch.py" -b "${r_args}"
+        done 
+    done
+done
+
