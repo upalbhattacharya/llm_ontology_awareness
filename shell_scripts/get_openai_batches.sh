0d6bc811810cc4f30add7d86024dc4ef7d56eab4
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-04-09 10:12:42 +0200

    shell_scripts/get_openai_batches.sh

---
+++ shell_scripts/get_openai_batches.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+SHOTS=(1 3 5 10)
+ONTOLOGIES=("astronomy-ontology" "case-uco-owl-trafficking" "wines-ontology")
+BASE_PATH="$HOME/Results/llm_ontology_awareness/term_typing/ranked_retrieval"
+MODEL="gpt-4o"
+
+for shot in "${SHOTS[@]}"; do
+    for ont in
