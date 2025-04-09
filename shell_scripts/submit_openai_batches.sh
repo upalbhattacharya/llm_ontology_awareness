4d18136014c0613cb5a7ca4f2024f4c45ae49d3f
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-04-09 15:17:08 +0200

    shell_scripts/submit_openai_batches.sh

--- shell_scripts/submit_openai_batches.sh
+++ shell_scripts/submit_openai_batches.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
 
-SHOTS=(2 4 6 7 8 9)
-ONTOLOGIES=("astronomy-ontology" "case-uco-owl-trafficking" "wines-ontology")
+SHOTS=(1)
+ONTOLOGIES=("astronomy-ontology" "case-uco-owl-trafficking")
 BASE_PATH="$HOME/Results/llm_ontology_awareness/term_typing/ranked_retrieval"
-MODEL="gpt-4o"
-STRAT_SUFFIX="/most_common"
+MODEL="o1-preview"
+# STRAT_SUFFIX="/most_common"
+STRAT_SUFFIX=""
 
 for shot in "${SHOTS[@]}"; do
     for ont in "${ONTOLOGIES[@]}"; do
-        if [ ${shot} -eq 2 ] && [ ${ont} == "wines-ontology" ]; then
-            echo "Skipping"
-            continue
-        fi
+        # if [ ${shot} -eq 2 ] && [ ${ont} == "wines-ontology" ]; then
+        #     echo "Skipping"
+        #     continue
+        # fi
         for r_args in "${BASE_PATH}/${shot}_shot${STRAT_SUFFIX}/${MODEL}/${ont}"/*; do
             echo ${r_args}
             python3 "${HOME}/PhD/Experiments/llm_ontology_awareness/src/llm_ontology_awareness/model/open_ai/batch/submit_batch.py" -b "${r_args}"
