39157054808ee24c71ad0dd106efbc339f70bdef
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-04-09 10:12:53 +0200

    shell_scripts/get_openai_batches.sh

--- shell_scripts/get_openai_batches.sh
+++ shell_scripts/get_openai_batches.sh
@@ -4,6 +4,7 @@ SHOTS=(1 3 5 10)
 ONTOLOGIES=("astronomy-ontology" "case-uco-owl-trafficking" "wines-ontology")
 BASE_PATH="$HOME/Results/llm_ontology_awareness/term_typing/ranked_retrieval"
 MODEL="gpt-4o"
+STRAT_SUFFIX="/most_common"
 
 for shot in "${SHOTS[@]}"; do
     for ont in
