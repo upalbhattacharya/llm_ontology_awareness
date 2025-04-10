6f910c6448c36d07428f282265d49c8a17cc2f2b
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-04-10 13:09:29 +0200

    shell_scripts/submit_openai_batches.sh

--- shell_scripts/submit_openai_batches.sh
+++ shell_scripts/submit_openai_batches.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-SHOTS=(0)
-ONTOLOGIES=("astronomy-ontology" "case-uco-owl-trafficking")
+SHOTS=(3)
+ONTOLOGIES=("wines-ontology" "case-uco-owl-trafficking")
 BASE_PATH="$HOME/Results/llm_ontology_awareness/term_typing/ranked_retrieval"
-MODEL="o1-preview"
-# STRAT_SUFFIX="/most_common"
-STRAT_SUFFIX=""
+MODELS=("gpt-4o-temp_high" "gpt-4o-temp_low")
+STRAT_SUFFIX="/most_common"
+# STRAT_SUFFIX=""
 
 for shot in "${SHOTS[@]}"; do
     for ont in "${ONTOLOGIES[@]}"; do
@@ -13,9 +13,11 @@ for shot in "${SHOTS[@]}"; do
         #     echo "Skipping"
         #     continue
         # fi
-        for r_args in "${BASE_PATH}/${shot}_shot${STRAT_SUFFIX}/${MODEL}/${ont}"/*; do
-            echo ${r_args}
-            python3 "${HOME}/PhD/Experiments/llm_ontology_awareness/src/llm_ontology_awareness/model/open_ai/batch/submit_batch.py" -b "${r_args}"
+        for MODEL in "${MODELS[@]}"; do
+            for r_args in "${BASE_PATH}/${shot}_shot${STRAT_SUFFIX}/${MODEL}/${ont}"/*; do
+                echo ${r_args}
+                # python3 "${HOME}/PhD/Experiments/llm_ontology_awareness/src/llm_ontology_awareness/model/open_ai/batch/submit_batch.py" -b "${r_args}"
+            done
         done 
     done
 done
