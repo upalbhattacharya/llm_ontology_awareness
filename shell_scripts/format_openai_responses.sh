b3b35795756c664824ef3d71ad3349024ba7e70d
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-04-10 16:33:04 +0200

    shell_scripts/format_openai_responses.sh

--- shell_scripts/format_openai_responses.sh
+++ shell_scripts/format_openai_responses.sh
@@ -11,7 +11,7 @@ for shot in "${SHOTS[@]}"; do
     for ont in "${ONTOLOGIES[@]}"; do
         for r_args in "${BASE_RESULTS_PATH}/${shot}_shot${STRAT_SUFFIX}/${MODEL}/${ont}"/*; do
             for run in "${r_args}/runs"/*; do
-                echo ${run}
+                # echo ${run}
                 # python3 ${HOME}/PhD/Experiments/llm_ontology_awareness/src/llm_ontology_awareness/model/open_ai/format_response_content/term_typing.py -f ${run} -r ${r_args}/params.json -l ${r_args}/label_mapping.json
             done
         done
