f7e5b733642de3ccb4bb336168ad90e960bb3d2f
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-04-10 13:03:56 +0200

    shell_scripts/create_openai_batches.sh

--- shell_scripts/create_openai_batches.sh
+++ shell_scripts/create_openai_batches.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 SHOTS=(0)
-ONTOLOGIES=("astronomy-ontology" "case-uco-owl-trafficking")
+ONTOLOGIES=("wines-ontology" "case-uco-owl-trafficking")
 RUN_ARGS_PATH="$HOME/PhD/Experiments/llm_ontology_awareness/run_args/term_typing/ranked_retrieval"
-MODEL="o1-preview"
-# STRAT_SUFFIX="/most_common"
-STRAT_SUFFIX=""
+MODELS=("gpt-4o-temp_high" "gpt-4o-temp_low")
+STRAT_SUFFIX="/most_common"
+# STRAT_SUFFIX=""
 
 for shot in "${SHOTS[@]}"; do
     for ont in "${ONTOLOGIES[@]}"; do
@@ -13,9 +13,11 @@ for shot in "${SHOTS[@]}"; do
         #     echo "Skipping"
         #     continue
         # fi
-        for r_args in "${RUN_ARGS_PATH}/${shot}_shot${STRAT_SUFFIX}/${MODEL}/${ont}"/*; do
-            echo ${r_args}
-            python3 "${HOME}/PhD/Experiments/llm_ontology_awareness/src/llm_ontology_awareness/model/open_ai/batch/create_batch/term_typing.py" -f "${r_args}"
+        for MODEL in "${MODELS[@]}"; do
+            for r_args in "${RUN_ARGS_PATH}/${shot}_shot${STRAT_SUFFIX}/${MODEL}/${ont}"/*; do
+                echo ${r_args}
+                # python3 "${HOME}/PhD/Experiments/llm_ontology_awareness/src/llm_ontology_awareness/model/open_ai/batch/create_batch/term_typing.py" -f "${r_args}"
+            done
         done 
     done
 done
