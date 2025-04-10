4a2a9f5adcfa1dbc1e524efcb081d68346ed6df9
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-04-10 16:44:42 +0200

    src/llm_ontology_awareness/model/common/format_response.py

--- src/llm_ontology_awareness/model/common/format_response.py
+++ src/llm_ontology_awareness/model/common/format_response.py
@@ -33,10 +33,15 @@ def deepseekr1(value: str):
     values = llama3(values)
     return values
 
+def gpt-4o(value: str):
+    values = value
+    return values
+
 
 llm_response_extract = {
     "meta-llama/Meta-Llama-3-8B-Instruct": llama3,
     "deepseek-ai/DeepSeek-R1-Distill-Llama-8B": deepseekr1,
+    "gpt-4o": gpt-4o,
 }
 
 
