60bdd3448075e9515d02d7170be6aab3b26754f9
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-04-11 12:11:38 +0200

    shell_scripts/format_huggingface_responses.sh

---
+++ shell_scripts/format_huggingface_responses.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+ARR=("astronomy-ontology" "case-uco-owl-trafficking" "wines-ontology")
+LLM="deepseekr1-distill-llama3-8B"
+shots=(0)
+RUN="run_2"
+for ont in "${ARR[@]}"; do
+	echo $ont
+	for i in "${shots[@]}"; do
+        echo ${i}
+		for j in "${HOME}/Results/llm_ontology_awareness/term_typing/ranked_retrieval/${i}_shot/${LLM}/${ont}"/*; do
+			uid=$(basename "${j}")
+			echo ${uid}
+			python3 $HOME/PhD/Experiments/llm_ontology_awareness/src/llm_ontology_awareness/model/hugging_face/format_response_content/term_typing.py -f ${j}/${RUN}/responses.json \
+				-l ${j}/${RUN}/label_mapping.json \
+				-r ${j}/${RUN}/params.json
+		done
+	done
+done
