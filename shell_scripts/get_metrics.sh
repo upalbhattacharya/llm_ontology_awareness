8c6036d4e57f359641e1ff3259ff3cfe0d05b8e8
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-04-11 14:06:37 +0200

    shell_scripts/get_metrics.sh

--- shell_scripts/get_metrics.sh
+++ shell_scripts/get_metrics.sh
@@ -16,7 +16,7 @@ RUN="run_1"
 
 for shot in "${SHOTS[@]}"; do
     for ont in "${ONTOLOGIES[@]}"; do 
-	    for r_args in "${HOME}/Results/llm_ontology_awareness/term_typing/ranked_retrieval/${shot}_shot${STRAT_SUFFIX}/${LLM}/${ONT}"/*; do
+	    for r_args in "${HOME}/Results/llm_ontology_awareness/term_typing/ranked_retrieval/${shot}_shot${STRAT_SUFFIX}/${LLM}/${ont[1]}"/*; do
 		    python3 ${HOME}/PhD/Experiments/llm_ontology_awareness/src/llm_ontology_awareness/metrics/results/term_typing.py \
                     -yt ${HOME}/Data/ontologies/${ONT}/data/term_typing/ranked_retrieval/${i}_shot${STRAT_SUFFIX}/${DATE}/term_typing_ranked_retrieval_dataset.json \
                     -yp ${j}/${RUN}/predictions.json \
