7ec61c4bbb645606e1a252aa133a24ed499c505f
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-04-11 12:26:04 +0200

    shell_scripts/format_huggingface_responses.sh

--- shell_scripts/format_huggingface_responses.sh
+++ shell_scripts/format_huggingface_responses.sh
@@ -13,7 +13,7 @@ for ont in "${ONTOLOGIES[@]}"; do
                 echo ${run_path}
 			    python3 $HOME/PhD/Experiments/llm_ontology_awareness/src/llm_ontology_awareness/model/hugging_face/format_response_content/term_typing.py -f ${run_path}/responses.json \
 				        -l ${run_path}/label_mapping.json \
-				        -r "${HOME}/PhD/Experiments/llm_ontology_awareness/run_args/term_typing/ranked_retrieval/${i}_shot${STRAT_SUFFIX}/${LLM}/${ont}/${uid}.json"
+				        -r "${HOME}/PhD/Experiments/llm_ontology_awareness/run_args/term_typing/ranked_retrieval/${shot}_shot${STRAT_SUFFIX}/${LLM}/${ont}/${uid}.json"
             done
 		done
 	done
