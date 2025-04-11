4979513db921ec591df426975f22fa37ba357b73
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-04-11 12:20:22 +0200

    shell_scripts/format_huggingface_responses.sh

--- shell_scripts/format_huggingface_responses.sh
+++ shell_scripts/format_huggingface_responses.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
-ARR=("astronomy-ontology" "case-uco-owl-trafficking" "wines-ontology")
-LLM="deepseekr1-distill-llama3-8B"
-shots=(0)
+ONTOLOGIES=("astronomy-ontology" "case-uco-owl-trafficking" "wines-ontology")
+LLM="llama3-7B"
+SHOTS=(3)
 RUN="run_2"
-for ont in "${ARR[@]}"; do
-	echo $ont
-	for i in "${shots[@]}"; do
-        echo ${i}
-		for j in "${HOME}/Results/llm_ontology_awareness/term_typing/ranked_retrieval/${i}_shot/${LLM}/${ont}"/*; do
+for ont in "${ONTOLOGIES[@]}"; do
+	for shot in "${SHOTS[@]}"; do
+		for j in "${HOME}/Results/llm_ontology_awareness/term_typing/ranked_retrieval/${shot}_shot/${LLM}/${ont}"/*; do
 			uid=$(basename "${j}")
 			echo ${uid}
 			python3 $HOME/PhD/Experiments/llm_ontology_awareness/src/llm_ontology_awareness/model/hugging_face/format_response_content/term_typing.py -f ${j}/${RUN}/responses.json \
