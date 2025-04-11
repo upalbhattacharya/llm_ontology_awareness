5ed0747a9328dad12c26d32b0b86c344be0161fd
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-04-11 14:09:32 +0200

    shell_scripts/get_metrics.sh

--- shell_scripts/get_metrics.sh
+++ shell_scripts/get_metrics.sh
@@ -5,6 +5,7 @@ ONTOLOGIES=(("wines-ontology" 4 "2024-09-05") ("astronomy-ontology" 10 "2024-11-
 # ONTOLOGIES=(("wines-ontology" 4 "2025-03-30") ("astronomy-ontology" 10 "2025-03-30") ("case-uco-owl-trafficking" 8 "2025-03-30"))
 
 LLM="llama3-7B"
+# LLM_SUFFIX="/runs"
 SHOTS=(0)
 STRAT_SUFFIX=""
 # STRAT_SUFFIX="/most_common"
@@ -15,6 +16,7 @@ DATA_BASE_PATH=$HOME/Data/ontologies
 for shot in "${SHOTS[@]}"; do
     for ont in "${ONTOLOGIES[@]}"; do 
 	    for r_args in "${RESULTS_BASE_PATH}/${shot}_shot${STRAT_SUFFIX}/${LLM}/${ont[1]}"/*; do
+            for run_path in "${r_args}${LLM_SUFFIX}"/*;
 		    python3 ${HOME}/PhD/Experiments/llm_ontology_awareness/src/llm_ontology_awareness/metrics/results/term_typing.py \
                     -yt "${DATA_BASE_PATH}/${ont[0]}/data/term_typing/ranked_retrieval/${shot}_shot${STRAT_SUFFIX}/${ont[2]}/term_typing_ranked_retrieval_dataset.json \
                     -yp ${j}/${RUN}/predictions.json \
