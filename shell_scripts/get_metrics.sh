83078543e7d346492227644915073d2be6bac29b
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-04-11 14:13:30 +0200

    shell_scripts/get_metrics.sh

--- shell_scripts/get_metrics.sh
+++ shell_scripts/get_metrics.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 ONTOLOGIES=(("wines-ontology" 4 "2024-09-05") ("astronomy-ontology" 10 "2024-11-04") ("case-uco-owl-trafficking" 8 "2024-09-05"))
+echo $ONTOLOGIES
 
 # ONTOLOGIES=(("wines-ontology" 4 "2025-03-30") ("astronomy-ontology" 10 "2025-03-30") ("case-uco-owl-trafficking" 8 "2025-03-30"))
 
