a2abff883f3fc2a8971aef9f5fc12ef294c3d798
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-04-11 12:24:59 +0200

    shell_scripts/format_huggingface_responses.sh

--- shell_scripts/format_huggingface_responses.sh
+++ shell_scripts/format_huggingface_responses.sh
@@ -8,7 +8,7 @@ STRAT_SUFFIX="/most_common"
 for ont in "${ONTOLOGIES[@]}"; do
 	for shot in "${SHOTS[@]}"; do
 		for r_args in "${HOME}/Results/llm_ontology_awareness/term_typing/ranked_retrieval/${shot}_shot${STRAT_SUFFIX}/${LLM}/${ont}"/*; do
-			uid=$(basename "${j}")
+			uid=$(basename "${r_args}")
             for run_path in "${r_args}"/*; do
                 echo ${run_path}
 			    python3 $HOME/PhD/Experiments/llm_ontology_awareness/src/llm_ontology_awareness/model/hugging_face/format_response_content/term_typing.py -f ${run_path}/responses.json \
