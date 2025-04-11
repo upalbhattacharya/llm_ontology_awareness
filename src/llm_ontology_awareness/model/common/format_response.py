878b090449cd2b77bcb8b15046a84737ac58a1dc
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-04-11 12:33:37 +0200

    src/llm_ontology_awareness/model/common/format_response.py

--- src/llm_ontology_awareness/model/common/format_response.py
+++ src/llm_ontology_awareness/model/common/format_response.py
@@ -14,7 +14,7 @@ def llama3(value: str):
     )  # For older response format
     values = re.sub(
         r"\<\|start_header_id\|\>assistant\<\|end_header_id\|\>(.*)\<\|eot_id\|\>",
-        r"\1",
+        r"\2",
         values,
     )  # For older response format
     print(values)
