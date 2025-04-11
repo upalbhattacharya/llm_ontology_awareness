4ec47084ca44dee1ad1a88b56fab0257f1c7847b
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-04-11 12:24:12 +0200

    shell_scripts/format_huggingface_responses.sh

--- shell_scripts/format_huggingface_responses.sh
+++ shell_scripts/format_huggingface_responses.sh
@@ -11,9 +11,9 @@ for ont in "${ONTOLOGIES[@]}"; do
 			uid=$(basename "${j}")
             for run_path in "${r_args}"/*; do
                 echo ${run_path}
-			    # python3 $HOME/PhD/Experiments/llm_ontology_awareness/src/llm_ontology_awareness/model/hugging_face/format_response_content/term_typing.py -f ${j}/${RUN}/responses.json \
-				#         -l ${j}/${RUN}/label_mapping.json \
-				#         -r "${HOME}/PhD/Experiments/llm_ontology_awareness/run_args/term_typing/ranked_retrieval/${i}_shot/${LLM}/${ont}/${uid}.json"
+			    python3 $HOME/PhD/Experiments/llm_ontology_awareness/src/llm_ontology_awareness/model/hugging_face/format_response_content/term_typing.py -f ${run_path}/responses.json \
+				        -l ${j}/${RUN}/label_mapping.json \
+				        -r "${HOME}/PhD/Experiments/llm_ontology_awareness/run_args/term_typing/ranked_retrieval/${i}_shot/${LLM}/${ont}/${uid}.json"
             done
 		done
 	done
