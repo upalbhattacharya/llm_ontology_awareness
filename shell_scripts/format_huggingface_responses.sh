5fbb5a3f991816a8e82263043e3a3420819456cc
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-04-13 13:49:37 +0200

    shell_scripts/format_huggingface_responses.sh

--- shell_scripts/format_huggingface_responses.sh
+++ shell_scripts/format_huggingface_responses.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-ONTOLOGIES=("case-uco-owl-trafficking" "wines-ontology")
-LLM="llama3-7B-temp_high"
-SHOTS=(3)
+ONTOLOGIES=("astronomy-ontology" "case-uco-owl-trafficking" "wines-ontology")
+LLM="deepseekr1-distill-llama3-8B"
+SHOTS=(0)
 # RUN="run_2"
 STRAT_SUFFIX="/most_common"
 
