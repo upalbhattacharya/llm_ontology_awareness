04879c2ec30109305f03d06e608eecf81637d0d1
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-04-28 15:37:20 +0200

    src/llm_ontology_awareness/process_data/embedding_generation/open_ai/batch/create_batch.py

--- src/llm_ontology_awareness/process_data/embedding_generation/open_ai/batch/create_batch.py
+++ src/llm_ontology_awareness/process_data/embedding_generation/open_ai/batch/create_batch.py
@@ -4,5 +4,7 @@ import polars as pl
 from llm_ontology_awareness.model.open_ai.datasets.term_typing import (
     TermTypingRankedRetrievalDataset,
 )
-from llm_ontology_awareness.model.open_ai.run_args import RunArguments
+from llm_ontology_awareness.process_data.embedding_generation.open_ai.run_args import (
+    RunArguments,
+)
 from tqdm import tqdm
