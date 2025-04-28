77f9a1c5c67c24b001f4533668af79eaf84bbbb7
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-04-28 15:45:29 +0200

    src/llm_ontology_awareness/process_data/embedding_generation/open_ai/batch/create_batch.py

--- src/llm_ontology_awareness/process_data/embedding_generation/open_ai/batch/create_batch.py
+++ src/llm_ontology_awareness/process_data/embedding_generation/open_ai/batch/create_batch.py
@@ -8,3 +8,10 @@ from llm_ontology_awareness.process_data.embedding_generation.open_ai.run_args i
     RunArguments,
 )
 from tqdm import tqdm
+
+
+def create_embedding_batch(test_data, run_args, **kwargs) -> (pl.DataFrame, dict):
+    tasks = []
+    label_mapping = []
+    num_samples = len(test_data)
+    test_data = iter(test_data)
