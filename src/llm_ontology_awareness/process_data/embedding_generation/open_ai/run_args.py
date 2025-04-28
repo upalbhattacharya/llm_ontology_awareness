9ead41c8ebce6451de61f7b4061257ba47f85888
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-04-28 15:32:05 +0200

    src/llm_ontology_awareness/process_data/embedding_generation/open_ai/run_args.py

--- src/llm_ontology_awareness/process_data/embedding_generation/open_ai/run_args.py
+++ src/llm_ontology_awareness/process_data/embedding_generation/open_ai/run_args.py
@@ -20,7 +20,8 @@ class RunArguments(BaseModel):
     )
     encoding_format: Optional[str] = Field(
         default="float", metadata={"help": "Format to return data"}
-        
+    )
+
     def model_post_init(self, __context):
 
         self.output_dir = "output" if self.output_dir is None else self.output_dir
