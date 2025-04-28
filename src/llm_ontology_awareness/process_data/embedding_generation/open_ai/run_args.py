4563e610b31c07f9c9e254c064a0ce79d5c710bc
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-04-28 15:30:18 +0200

    src/llm_ontology_awareness/process_data/embedding_generation/open_ai/run_args.py

---
+++ src/llm_ontology_awareness/process_data/embedding_generation/open_ai/run_args.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+
+class RunArguments(BaseModel):
+    input: Optional[str] = Field(
+        default=None, metadata={"help": "Dataset file to load"}
+    )
+    output_dir: Optional[str] = Field(
+        default=None, metadata={"help": "Directory to save run data"}
+    )
+    model: Optional[str] = Field(
+        default="text-embedding-3-large", metadata={"help": "Embedding model to use"}
+    )
+    dimensions: Optional[int] = Field(
+        default=1024, metadata={"help": "Embedding dimension"}
+    )
